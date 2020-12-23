7a73e7b255cf3dd5a8537b6f478f92e3f3633b2c
zainabelsayed <eng.zainab92@gmail.com>
2020-12-23 02:38:47 +0200

    update contactus

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.post('/contact',(req,res)=>{
         }else{
         res.send(`<script>alert("Email Sent Successfully.")</script>`);
         console.log("Message sent");
-        //res.redirect('/')
+        res.redirect('/contactUs')
         }
 
     })

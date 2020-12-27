7c5fc9a0bd5e1f8026fc65680b04df8e162abead
zainabelsayed <eng.zainab92@gmail.com>
2020-12-27 22:47:48 +0200

    update

--- website/app.js
+++ website/app.js
@@ -14,7 +14,7 @@ function initMap(){
     })
 }
 
-const formEvent = document.addEventListener("submit",(event)=>{
+const formEvent = form.addEventListener("submit",(event)=>{
     event.preventDefault()
     let mail = new FormData(form)
     console.log(form)

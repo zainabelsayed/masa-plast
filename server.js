a14e4665c1b927eedf1bee9586129a6450c9a152
zainabelsayed <eng.zainab92@gmail.com>
2021-01-05 04:08:42 +0200

    'update'

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ oauth2Client.setCredentials({
 })
 const accessToken = oauth2Client.getAccessToken()
 const app = express()
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(express.static('website'))
 /*app.get('/', function(req, res){
